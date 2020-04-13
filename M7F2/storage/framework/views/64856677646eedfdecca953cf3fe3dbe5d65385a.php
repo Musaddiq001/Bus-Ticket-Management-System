f41f98c06151ea402cf452a1bf9cb2dc65f4c383
Musaddiq001 <musaddiqmk19@gmail.com>
2020-04-14 04:26:51 +0600

    M7F2

---
+++ M7F2/storage/framework/views/64856677646eedfdecca953cf3fe3dbe5d65385a.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home page</title>
+</head>
+<body>	
+
+	<h1>Bus managers!</h1>&nbsp
+	<a href="<?php echo e(route('home.index')); ?>">back</a> |
+	<a href="/logout">Logout</a> 
+
+	<table border="1">
+		<tr>
+			<th>ID</th>
+			<th>USERNAME</th>
+			<th>EMAIL</th>
+			<th>TYPE</th>
+		</tr>
+		
+		<?php $__currentLoopData = $users; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $user): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+		<tr>
+			<td><?php echo e($user['userId']); ?></td>
+			<td><?php echo e($user['username']); ?></td>
+			<td><?php echo e($user['email']); ?></td>
+			<td><?php echo e($user['type']); ?></td>
+			<td>
+				<a href="<?php echo e(route('home.edit', $user['userId'])); ?>">Edit</a> | 
+				<a href="<?php echo e(route('home.delete', $user['userId'])); ?>">Delete</a> |
+				<a href="<?php echo e(route('home.show', $user['userId'])); ?>">Details</a> 
+			</td>
+		</tr>
+		<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+	</table>
+
+</body>
+</html><?php /**PATH C:\xampp\htdocs\aaaa\btrs\resources\views/home/view_users.blade.php ENDPATH**/ ?>
