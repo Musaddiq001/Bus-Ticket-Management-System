3186476a976104f6be7b6b1be906d4aca3726107
Musaddiq001 <musaddiqmk19@gmail.com>
2020-04-22 12:52:54 +0600

    M22 FUll

---
+++ M22 Full/resources/views/home/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home page</title>
+</head>
+<body>	
+
+	<h1>Welcome Btms!</h1>&nbsp
+
+	<a href="{{route('home.list')}}">View Managers</a> |
+	<a href="{{route('home.list1')}}">View Support Staff</a> |
+	<a href="{{route('home.list2')}}">View Bus Counters</a> |
+	<a href="{{route('home.list3')}}">View Buses list</a> |
+	<a href="{{route('home.add')}}">Add new bus</a> |
+	
+	<a href="/logout">Logout</a> 
+
+</body>
+</html>
